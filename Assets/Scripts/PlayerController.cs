3109928ffd900350f48ab09bfa96c76091136702
Trictan <68138473+Trictan@users.noreply.github.com>
2026-02-03 01:22:38 +0100

    Animations (Idle, Walk)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,22 +5,38 @@ public class PlayerCtrl :MonoBehaviour
     public float moveSpeed = 5f;
 
     private Rigidbody2D rb;
-    private float speedX;
-    private float speedY;
+    private Animator animator;
+    private Vector2 moveVector;
+
 
     void Start ()
     {
+        // fetch components
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update ()
     {
-        speedX = Input.GetAxisRaw("Horizontal");
-        speedY = Input.GetAxisRaw("Vertical");
+        // get movement input
+        float speedX = Input.GetAxisRaw("Horizontal");
+        float speedY = Input.GetAxisRaw("Vertical");
+        // create and normalize direction vector
+        moveVector = new Vector2(speedX, speedY).normalized;
+
+        // update animation direction if direction is non-zero
+        if (moveVector != Vector2.zero) {
+            animator.SetFloat("speedX", speedX);
+            animator.SetFloat("speedY", speedY);
+            animator.SetBool("isWalking", true);
+        } else
+        {
+            animator.SetBool("isWalking", false);
+        }
     }
 
     void FixedUpdate ()
     {
-        rb.linearVelocity = new Vector3(speedX, speedY).normalized * moveSpeed;
+        rb.linearVelocity = moveVector * moveSpeed;
     }
 }
