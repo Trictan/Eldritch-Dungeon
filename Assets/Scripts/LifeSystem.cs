a397b3ee38c00db846a95defe2fe80b2329fad47
Trictan <68138473+Trictan@users.noreply.github.com>
2026-02-05 17:19:37 +0100

    Bugfix

--- Assets/Scripts/LifeSystem.cs
+++ Assets/Scripts/LifeSystem.cs
@@ -2,26 +2,21 @@ using UnityEngine;
 using Unity.VisualScripting;
 
 public class LifeSystem : MonoBehaviour
-{
-    private float currentHealth;
+{   
+    public float maxHealth;
+    float currentHealth;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        float maxHealth = (float) Variables.Object(this.gameObject).Get("maxHealth");
-        float currentHealth = (float) Variables.Object(this.gameObject).Get("Health");
-        print(currentHealth);
+        currentHealth=maxHealth;
     }
 
     // Update is called once per frame
     public void TakeDamage(float damage)
     {
 
-        print(damage);
         currentHealth=currentHealth - damage;
-        print(currentHealth);
-        Variables.Object(this.gameObject).Set("currentHealth",currentHealth);
-        print(currentHealth);
         Debug.Log(gameObject.name + " took " + damage + " damage. Health: " + currentHealth);
 
         if(currentHealth <= 0)
