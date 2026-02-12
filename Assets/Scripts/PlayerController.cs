455bd92035730b9e4d6078cac5eb32c61bbaccbe
Trictan <68138473+Trictan@users.noreply.github.com>
2026-02-12 18:46:04 +0100

    GameController, RoomController overhaul

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 
-public class PlayerCtrl :MonoBehaviour
+public class PlayerCtrl: MonoBehaviour
 {
     public RoomControllerScript roomControllerScript;
     public float moveSpeed = 3f;
