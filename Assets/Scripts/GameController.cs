2627337884bf2fe085d0eed2ff12a7a27f87f944
Trictan <68138473+Trictan@users.noreply.github.com>
2026-02-12 15:04:26 +0100

    Small fixes

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private int floor;
+    private int traversedRooms;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        floor=0;
+        traversedRooms=0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
