041308de0a2a03da41771fa88f6b3323143b2bb0
Trictan <68138473+Trictan@users.noreply.github.com>
2026-03-08 18:55:38 +0100

    Player correction

--- Assets/Scripts/AIChase.cs
+++ Assets/Scripts/AIChase.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class AIChase : monobehaviour
+public class AIChase : MonoBehaviour
 {
     private GameObject player;
     public float speed;
@@ -19,12 +19,12 @@ public class AIChase : monobehaviour
     }
     void FixedUpdate()
     {
-        distance = Vector2.Distance(transform.position, playa.transform.position);
-        Vector2 direction = playa.transform.position-transform.position;
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position-transform.position;
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
 
-        transform.position=Vector2.MoveTowards(this.transform.position, playa.transform.position, speed*Time.deltaTime);
+        transform.position=Vector2.MoveTowards(this.transform.position, player.transform.position, speed*Time.deltaTime);
        // transform.rotation = Quaternion.Euler(Vector3.forward*angle);
     }
 }
