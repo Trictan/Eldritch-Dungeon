5e669bb1e948723082b0eb22aeb5f2277ba5ef94
vilm888 <lundqvistvilmer@gmail.com>
2026-03-08 19:43:28 +0100

    wasd

--- Assets/Scripts/tungtungskibscob.cs
+++ Assets/Scripts/tungtungskibscob.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class MonoBehaviour : MonoBehaviour
+public class tung : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
