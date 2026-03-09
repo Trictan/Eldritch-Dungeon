555eeb14ecc9914c55aa2d805dbc09be1cb68df2
Mille <182352340+Mille-code9@users.noreply.github.com>
2026-03-09 17:04:26 +0100

    Game Over text

--- Assets/GameOverTextUI.cs
+++ Assets/GameOverTextUI.cs
@@ -4,12 +4,11 @@ using TMPro;
 public class GameOverTextUI : MonoBehaviour
 {
     public TextMeshProUGUI textElement;
-    public int skib;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        textElement.text = "Game Over!\nYou traversed " + GameController.traversedRooms+" rooms on floor "+ GameController.floor;
+        textElement.text = "You traversed " + GameController.traversedRooms+" rooms on floor "+ GameController.floor;
     }
 
     // Update is called once per frame
